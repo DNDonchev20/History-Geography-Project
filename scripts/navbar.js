6da9de2cd7aa394b4f279ff219d589b01f4ae463
DNDonchev20 <DNDonchev20@codingburgas.bg>
2022-03-04 17:28:29 +0200

    Fix login and register responsiveness

--- scripts/navbar.js
+++
@@ -1,8 +0,0 @@
-function myFunction() {
-    var x = document.getElementById("topnav");
-    if (x.className === "navbar") {
-        x.className += " responsive";
-    } else {
-        x.className = "navbar";
-    }
-  }
