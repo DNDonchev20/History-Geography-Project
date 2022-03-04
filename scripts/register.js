d8135fb09430e5bd331606f012ecb8178186083e
Nikolay Despotov <85335497+NVDespotov20@users.noreply.github.com>
2022-03-04 19:44:00 +0200

    Add register validation

--- scripts/register.js
+++ scripts/register.js
@@ -4,16 +4,60 @@ function register() {
   userCount = Number(localStorage.getItem("Users Count"));
   localStorage.setItem("Users Count", userCount);
 
+  if (
+    !checkPass(
+      document.getElementById("Pass").value,
+      document.getElementById("confirmPass").value
+    )
+  ) {
+    alert("Password and Confirm Password don't match!\nPlease try Again!");
+    return false;
+  }
+
   let user = new User(
     document.getElementById("Username").value,
     document.getElementById("Email").value,
     document.getElementById("Pass").value
   );
 
-  let user_stringified = JSON.stringify(user);
-  localStorage.setItem("#" + userCount + "User", user_stringified);
+  if (availableUsername(user.Username) && availableEmail(user.Email)) {
+    let user_stringified = JSON.stringify(user);
+    localStorage.setItem("#" + userCount + "User", user_stringified);
 
-  ++userCount;
-  localStorage.setItem("Users Count", userCount);
+    ++userCount;
+    localStorage.setItem("Users Count", userCount);
+    return true;
+  } else if (availableUsername(user.Username) == false) {
+    alert("An account with that username already exists!");
+    return false;
+  } else if (availableEmail(user.Email) == false) {
+    alert("An account with that Email already exists!");
+    return false;
+  }
+}
+
+function availableUsername(nameToCheck) {
+  let user;
+  for (var i = 0; i < userCount; i++) {
+    user = JSON.parse(localStorage.getItem("#" + i + "User"));
+    if (nameToCheck == user.Username) {
+      return false;
+    }
+  }
   return true;
+}
+
+function availableEmail(emailToCheck) {
+  let user;
+  for (var i = 0; i < userCount; i++) {
+    user = JSON.parse(localStorage.getItem("#" + i + "User"));
+    if (emailToCheck == user.Email) {
+      return false;
+    }
+  }
+  return true;
+}
+
+function checkPass(pass, confirmPass) {
+  return pass == confirmPass;
 }
