e100f03ab086459c353133aa1fb322f64ac7ba59
Nikolay Despotov <85335497+NVDespotov20@users.noreply.github.com>
2022-03-06 21:07:38 +0200

    Add Star class. Close #20.

---
+++ scripts/Star.js
@@ -0,0 +1,23 @@
+class Star {
+  constructor(
+    cx,
+    cy,
+    outerRadius,
+    innerRadius,
+    numPoints,
+    lineWidth,
+    stroke,
+    fill,
+    rotate
+  ) {
+    this.cx = cx;
+    this.cy = cy;
+    this.outerRadius = outerRadius;
+    this.innerRadius = innerRadius;
+    this.numPoints = numPoints;
+    this.lineWidth = lineWidth;
+    this.stroke = stroke;
+    this.fill = fill;
+    this.rotate = rotate;
+  }
+}
--- scripts/map.js
+++ scripts/map.js
@@ -1,102 +1,86 @@
 var drawStar = function (context, o = {}) {
-    let numPoints = o.numPoints || 5;
-    let outerRadius = o.outerRadius || 100;
-    let innerRadius = o.innerRadius || 50;
-    let cx = o.cx || 0;
-    let cy = o.cy || 0;
-  
-    let rotate = o.rotate || 0;
-  
-    context.lineWidth = o.lineWidth || 1;
-    context.strokeStyle = o.stroke || "#000";
-    context.beginPath();
-    const draw = (radius, angle, action) => {
-      let x = cx + radius * Math.cos(angle);
-      let y = cy + radius * Math.sin(angle);
-      context[action](x, y);
-    };
-  
-    draw(outerRadius, rotate, "moveTo");
-  
-    let angle = (2 * Math.PI) / numPoints;
-  
-    for (var i = 0; i <= numPoints; i++) {
-      let outerAngle = i * angle + rotate;
-      let innerAngle = outerAngle + angle / 2;
-      draw(outerRadius, outerAngle, "lineTo");
-      draw(innerRadius, innerAngle, "lineTo");
-    }
-  
-    context.stroke();
-  
-    if (o.fill) {
-      context.fillStyle = o.fill;
-      context.fill();
-    }
-  
-    return {
-      left: cx - innerRadius,
-      top: cy - innerRadius,
-      right: cx + innerRadius,
-      bottom: cy + innerRadius,
-    };
+  let numPoints = o.numPoints || 5;
+  let outerRadius = o.outerRadius || 100;
+  let innerRadius = o.innerRadius || 50;
+  let cx = o.cx || 0;
+  let cy = o.cy || 0;
+
+  let rotate = o.rotate || 0;
+
+  context.lineWidth = o.lineWidth || 1;
+  context.strokeStyle = o.stroke || "#000";
+  context.beginPath();
+  const draw = (radius, angle, action) => {
+    let x = cx + radius * Math.cos(angle);
+    let y = cy + radius * Math.sin(angle);
+    context[action](x, y);
   };
-  
-  function drawMap(mapId, starDraw, info) {
-    const canvas = document.getElementById(mapId);
-    const context = canvas.getContext("2d");
-    const items = starDraw(context); //make the items clickable
-  
-    const map = document.getElementById(mapId);
-    map.addEventListener("click", function (e) {
-      let clickedX = e.pageX - this.offsetLeft;
-      let clickedY = e.pageY - this.offsetTop;
-  
-      for (let i = 0; i < items.length; i++) {
-        if (
-          clickedX < items[i].right &&
-          clickedX > items[i].left &&
-          clickedY > items[i].top &&
-          clickedY < items[i].bottom
-        ) {
-          alert(info[i].text);
-        }
-      }
-    });
+
+  draw(outerRadius, rotate, "moveTo");
+
+  let angle = (2 * Math.PI) / numPoints;
+
+  for (var i = 0; i <= numPoints; i++) {
+    let outerAngle = i * angle + rotate;
+    let innerAngle = outerAngle + angle / 2;
+    draw(outerRadius, outerAngle, "lineTo");
+    draw(innerRadius, innerAngle, "lineTo");
   }
-  
-  function render() {
-      //add more drawMap calls to draw each map
-    drawMap("map1", //mapId
+
+  context.stroke();
+
+  if (o.fill) {
+    context.fillStyle = o.fill;
+    context.fill();
+  }
+
+  return {
+    left: cx - innerRadius,
+    top: cy - innerRadius,
+    right: cx + innerRadius,
+    bottom: cy + innerRadius,
+  };
+};
+
+function drawMap(mapId, starDraw, info) {
+  const canvas = document.getElementById(mapId);
+  const context = canvas.getContext("2d");
+  const items = starDraw(context); //make the items clickable
+
+  const map = document.getElementById(mapId);
+  map.addEventListener("click", function (e) {
+    let clickedX = e.pageX - this.offsetLeft;
+    let clickedY = e.pageY - this.offsetTop;
+
+    for (let i = 0; i < items.length; i++) {
+      if (
+        clickedX < items[i].right &&
+        clickedX > items[i].left &&
+        clickedY > items[i].top &&
+        clickedY < items[i].bottom
+      ) {
+        alert(info[i].text);
+      }
+    }
+  });
+}
+
+function render() {
+  //add more drawMap calls to draw each map
+  drawMap(
+    "map1", //mapId
     function (context) {
       const items = [];
+
+      const Star1 = new Star(500, 300, 25, 10, 5, 4, "#f06d06", "#fed8b1", 60);
       //star drawing begin
-      const star1 = drawStar(context, {
-        cx: 500,
-        cy: 300,
-        outerRadius: 25,
-        innerRadius: 10,
-        numPoints: 5,
-        lineWidth: 4,
-        stroke: "#f06d06",
-        fill: "#fed8b1",
-        rotate: 60,
-      });
+      console.log(Star1);
+      const star1 = drawStar(context, Star1);
       items.push(star1); //push the drawn star to the items list
-  
-      const star2 = drawStar(context, {
-        cx: 200,
-        cy: 100,
-        outerRadius: 25,
-        innerRadius: 10,
-        numPoints: 5,
-        lineWidth: 4,
-        stroke: "#f06d06",
-        fill: "#fed8b1",
-        rotate: 60,
-      });
+      const Star2 = new Star(200, 100, 25, 10, 5, 4, "#f06d06", "#fed8b1", 60);
+      const star2 = drawStar(context, Star2);
       items.push(star2);
-  
+
       return items;
     },
     [
@@ -113,7 +97,6 @@ var drawStar = function (context, o = {}) {
         text: "Country 3 info",
       },
     ]
-    );
-  
-  }
-  window.addEventListener("DOMContentLoaded", render);
+  );
+}
+window.addEventListener("DOMContentLoaded", render);
