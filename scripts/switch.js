0f920b44ea591b053bdedd9881ab33eb40c45277
MIZlatev20 <mizlatev20@codingburgas.bg>
2022-03-18 21:19:57 +0200

    Fix switch maps method bug. Close #33.

--- scripts/switch.js
+++ scripts/switch.js
@@ -1,25 +1,25 @@
 function switchMap(arg){
     switch (arg) {
         case "e39":
-            document.getElementById("map1").style.backgroundImage = "url('../../images/europe_1939.svg')";
+            document.getElementById("map1").style.backgroundImage = "url(images/europe_1939.svg)";
             document.querySelector('#map1').scrollIntoView({
                 behavior: 'smooth'
             });
             break;
         case "e45":
-            document.getElementById("map1").style.backgroundImage = "url('../../images/europe_1945.svg')";
+            document.getElementById("map1").style.backgroundImage = "url(images/europe_1945.svg)";
             document.querySelector('#map1').scrollIntoView({
                 behavior: 'smooth'
             });
             break;
         case "a39":
-            document.getElementById("map2").style.backgroundImage = "url('../../images/asia_1939.svg')";
+            document.getElementById("map2").style.backgroundImage = "url(images/asia_1939.svg)";
             document.querySelector('#map2').scrollIntoView({
                 behavior: 'smooth'
             });
             break;
         case "a45":
-            document.getElementById("map2").style.backgroundImage = "url('../../images/asia_1945.svg')";
+            document.getElementById("map2").style.backgroundImage = "url(images/asia_1945.svg)";
             document.querySelector('#map2').scrollIntoView({
                 behavior: 'smooth'
             });
